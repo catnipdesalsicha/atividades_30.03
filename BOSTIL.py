211fd99d96670985d44c63c48cfebe0970db34a5
duda :0 <107702701+catnipdesalsicha@users.noreply.github.com>
2023-03-30 10:17:50 -0300

    Update BOSTIL.py

---
+++ BOSTIL.py
@@ -0,0 +1,10 @@
+print("------------WELCOME-------------------")
+
+sigla = input("Digite uma sigla ")
+
+if sigla =='SP' or sigla =='RJ':
+    print("Parabéns você é um sudestino safado")
+elif sigla == 'PB' or sigla=='PE':
+    print("Parabéns você é arretado")
+else:
+    print("Tais no canto errado visse")
